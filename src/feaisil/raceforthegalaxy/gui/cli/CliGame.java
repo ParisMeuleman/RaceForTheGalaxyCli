e40c60fc6f644437b8110c72e763459a20c7d5a0
Paris MEULEMAN <paris.meuleman@gmail.com>
2013-06-07 08:42:09 +0200

    Develop and explore phases

--- src/feaisil/raceforthegalaxy/gui/cli/CliGame.java
+++ src/feaisil/raceforthegalaxy/gui/cli/CliGame.java
@@ -7,33 +7,33 @@ import feaisil.raceforthegalaxy.Expansion;
 import feaisil.raceforthegalaxy.game.Game;
 import feaisil.raceforthegalaxy.game.Player;
 
-public class CliGame {
+public class CliGame
+{
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception{
-		
+  /**
+   * @param args
+   */
+  public static void main(String[] args) throws Exception
+  {
 
-		ArrayList<Expansion> expList = new ArrayList<Expansion>();
-		expList.add(Expansion.BaseGame);
-		Game.Configuration aConfig = new Game.Configuration(
-				expList,
-				false,
-				false,
-				false, 
-				new FileInputStream("res/raw/rftg_card_reference"));
-		Game aGame = new Game(aConfig);
+    ArrayList<Expansion> expList = new ArrayList<Expansion>();
+    expList.add(Expansion.BaseGame);
+//    expList.add(Expansion.TheBrinkOfWard);
+//    expList.add(Expansion.TheGatheringStorm);
+//    expList.add(Expansion.RebelVsImperium);
+    Game.Configuration aConfig = new Game.Configuration(expList, false, false,
+        false, new FileInputStream("res/raw/rftg_card_reference"));
+    Game aGame = new Game(aConfig);
 
-		CommandLineInterface cli = new CommandLineInterface(aGame);
-		@SuppressWarnings("unused")
-		Player aPl1 = new Player(aGame, "God", cli);
-		@SuppressWarnings("unused")
-		Player aPl2 = new Player(aGame, "Blatteman", cli);
-		
-		aGame.playGame();
-		
-		System.out.println(aGame);
-	}
+    CommandLineInterface cli = new CommandLineInterface(aGame);
+    @SuppressWarnings("unused")
+    Player aPl1 = new Player(aGame, "God", cli);
+    @SuppressWarnings("unused")
+    Player aPl2 = new Player(aGame, "Blatteman", cli);
+
+    aGame.playGame();
+
+    System.out.println(aGame);
+  }
 
 }
--- src/feaisil/raceforthegalaxy/gui/cli/CommandLineInterface.java
+++ src/feaisil/raceforthegalaxy/gui/cli/CommandLineInterface.java
@@ -13,148 +13,172 @@ import feaisil.raceforthegalaxy.gui.PlayerUserInterface;
 import feaisil.raceforthegalaxy.gui.QueryType;
 import feaisil.raceforthegalaxy.gui.Warning;
 
-public class CommandLineInterface implements PlayerUserInterface {
-	private Game gameRef;
-	private Player currentPlayer;
-	
-	public CommandLineInterface(Game gameRef) {
-		super();
-		this.gameRef = gameRef;
-		currentPlayer = null;
-	}
+public class CommandLineInterface implements PlayerUserInterface
+{
+  private Game   gameRef;
+  private Player currentPlayer;
 
-	public void displayCards(List<Card> iCards)
-	{
-		for(int i = 0; i < iCards.size(); i++)
-			System.out.println(" " + (i) + " - "+ iCards.get(i).toString());
-	}
+  public CommandLineInterface(Game gameRef)
+  {
+    super();
+    this.gameRef = gameRef;
+    currentPlayer = null;
+  }
 
-	public List<Card> getChoosenCards(List<Card> iCards, int iMin, int iMax) {
-		System.out.println("Choose " + iMin + " up to " + iMax + " cards");
-		System.out.println("Enter the number of the cards you want to choose (format: N N N)");
+  public void displayCards(List<Card> iCards)
+  {
+    for (int i = 0; i < iCards.size(); i++)
+      System.out.println(" " + (i) + " - " + iCards.get(i).toString());
+  }
 
-		List<Card> aResult = null;
-		
-		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-		
-		BufferedReader reader = new BufferedReader(inputStreamReader);
-		String aBuffer = "";
-		try {
-			while(true){
-				aResult= new ArrayList<Card>();
-				aBuffer = reader.readLine();
-				for(String aStr: aBuffer.split("[^\\d]"))
-				{
-					try
-					{
-						if( Integer.parseInt(aStr) < iCards.size())
-							aResult.add(iCards.get( Integer.parseInt(aStr)));
-					}
-					catch(java.lang.NumberFormatException iEx){
-						// Input not a number...
-					}
-				}
-				if(aResult.size() >= iMin && aResult.size() <= iMax)
-					break;
-				System.out.println("Invalid input, please try again.");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return aResult;
-	}
+  public List<Card> getChoosenCards(List<Card> iCards, int iMin, int iMax)
+  {
+    System.out.println("Choose " + iMin + " up to " + iMax + " cards");
+    System.out
+        .println("Enter the number of the cards you want to choose (format: N N N)");
 
-	public void switchToPlayer(Player iP)
-	{
-		if(iP.equals(currentPlayer))
-			return;
-		currentPlayer = iP;
-		
-		for(int i=0; i<10; i++)
-			System.out.println();
-		for(int i=0; i<20; i++)
-			System.out.print("=");
-		System.out.println();
-		System.out.println("TURN "+gameRef.getTurn());
-		System.out.println("* Game stats:");
-		System.out.println("Deck size: "+gameRef.getDeck());
-		System.out.println("Discard size: "+gameRef.getDiscardPile());
-		System.out.println("Remaining Vps: "+gameRef.getRemainingVp());
-		System.out.println("Remaining goals: "+gameRef.getGoals());
-		System.out.println();
-		for(Player p: gameRef.getPlayers())
-		{
-			if(!p.equals(currentPlayer))
-			{
-				System.out.println(p.getColor().toString()+" player:");
-				System.out.println("Vps: "+p.getVictoryPoints() + ", hand size " + p.getHand().size());
-				System.out.println("board: ");
-				displayCards(currentPlayer.getBoard());
-			}
-		}
-		System.out.println();
-		for(int i = 0; i<("* Player "+currentPlayer.getClientId()+" *").length(); i++)
-			System.out.print("*");
-		System.out.println();
-		System.out.println("* Player "+currentPlayer.getClientId()+" *");
-		for(int i = 0; i<("* Player "+currentPlayer.getClientId()+" *").length(); i++)
-			System.out.print("*");
-		System.out.println();
-		System.out.println("Player board: ");
-		displayCards(currentPlayer.getBoard());
-		System.out.println();
-		System.out.println("Player hand: ");
-		displayCards(currentPlayer.getHand());
-		System.out.println();
-	}
+    List<Card> aResult = null;
 
-	@Override
-	public List<Card> query(Player iP, QueryType query, List<Card> cards, int minCards,
-			int maxCards) {
-		
-		switchToPlayer(iP);
-		
-		switch(query)
-		{
-		case startingPhaseChooseWorld:
-			System.out.println("Choose your starting world");
-			break;
-		case startingPhaseDiscardHand:
-			System.out.println("Discard two cards from your starting hand");
-			break;
-		case chooseAction:
-			System.out.println("Choose an action to play");
-			break;
-		case exploreDiscard:
-			System.out.println("Explore phase, discard cards");
-			break;
-		case finalizeDiscardHand:
-			System.out.println("End of turn, discard cards from hand");
-			break;			
-		default:
-		}
-		displayCards(cards);
-		
-		return getChoosenCards(cards, minCards, maxCards);
-	}
+    InputStreamReader inputStreamReader = new InputStreamReader(System.in);
 
-	@Override
-	public void sendWarning(Player iP, Warning warning) {
-		switchToPlayer(iP);
-		
-		System.out.print("/!\\ ");
-		switch(warning)
-		{
-		case PrestigeActionAlreadyUsed:
-			System.out.print("Prestige action already used!");
-			break;
-		case TwoActionSelectedButNotPrestige:
-			System.out.print("Two actions selected but none prestige!");
-			break;
-		default:
-			System.out.print("Unknown warning");
-		}
-		System.out.println(" /!\\");
-		
-	}
+    BufferedReader reader = new BufferedReader(inputStreamReader);
+    String aBuffer = "";
+    try
+    {
+      while (true)
+      {
+        aResult = new ArrayList<Card>();
+        aBuffer = reader.readLine();
+        for (String aStr : aBuffer.split("[^\\d]"))
+        {
+          try
+          {
+            if (Integer.parseInt(aStr) < iCards.size())
+              aResult.add(iCards.get(Integer.parseInt(aStr)));
+          } catch (java.lang.NumberFormatException iEx)
+          {
+            // Input not a number...
+          }
+        }
+        if (aResult.size() >= iMin && aResult.size() <= iMax)
+          break;
+        System.out.println("Invalid input, please try again.");
+      }
+    } catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    return aResult;
+  }
+
+  public void switchToPlayer(Player iP)
+  {
+    if (iP.equals(currentPlayer))
+      return;
+    currentPlayer = iP;
+
+    for (int i = 0; i < 10; i++)
+      System.out.println();
+    for (int i = 0; i < 20; i++)
+      System.out.print("=");
+    System.out.println();
+    System.out.println("TURN " + gameRef.getTurn());
+    System.out.println("* Game stats:");
+    System.out.println("Deck size: " + gameRef.getDeck());
+    System.out.println("Discard size: " + gameRef.getDiscardPile());
+    System.out.println("Remaining Vps: " + gameRef.getRemainingVp());
+    System.out.println("Remaining goals: " + gameRef.getGoals());
+    System.out.println();
+    for (Player p : gameRef.getPlayers())
+    {
+      if (!p.equals(currentPlayer))
+      {
+        System.out.println(p.getColor().toString() + " player:");
+        System.out.println("Vps: " + p.getVictoryPoints() + ", hand size "
+            + p.getHand().size());
+        System.out.println("board: ");
+        displayCards(currentPlayer.getBoard());
+      }
+    }
+    System.out.println();
+    for (int i = 0; i < ("* Player " + currentPlayer.getClientId() + " *")
+        .length(); i++)
+      System.out.print("*");
+    System.out.println();
+    System.out.println("* Player " + currentPlayer.getClientId() + " *");
+    for (int i = 0; i < ("* Player " + currentPlayer.getClientId() + " *")
+        .length(); i++)
+      System.out.print("*");
+    System.out.println();
+    System.out.println("Player board: ");
+    displayCards(currentPlayer.getBoard());
+    System.out.println();
+    System.out.println("Player hand: ");
+    displayCards(currentPlayer.getHand());
+    System.out.println();
+  }
+
+  @Override
+  public List<Card> query(Player iP, QueryType query, List<Card> cards,
+      int minCards, int maxCards)
+  {
+
+    switchToPlayer(iP);
+
+    switch (query)
+    {
+      case startingPhaseChooseWorld:
+        System.out.println("Choose your starting world");
+        break;
+      case startingPhaseDiscardHand:
+        System.out.println("Discard two cards from your starting hand");
+        break;
+      case chooseAction:
+        System.out.println("Choose an action to play");
+        break;
+      case exploreDiscard:
+        System.out.println("Explore phase, discard cards");
+        break;
+      case developChoose:
+        System.out.println("You may choose a card to develop");
+        break;
+      case developDiscard:
+        System.out.println("Choose paiement for your development");
+        break;
+      case settleChoose:
+        System.out.println("You may choose a card to settle");
+        break;
+      case settleDiscard:
+        System.out.println("Explore phase, discard cards");
+        break;
+      case finalizeDiscardHand:
+        System.out.println("Too much cards in hand, discard cards");
+        break;
+      default:
+    }
+    displayCards(cards);
+
+    return getChoosenCards(cards, minCards, maxCards);
+  }
+
+  @Override
+  public void sendWarning(Player iP, Warning warning)
+  {
+    switchToPlayer(iP);
+
+    System.out.print("/!\\ ");
+    switch (warning)
+    {
+      case PrestigeActionAlreadyUsed:
+        System.out.print("Prestige action already used!");
+        break;
+      case TwoActionSelectedButNotPrestige:
+        System.out.print("Two actions selected but none prestige!");
+        break;
+      default:
+        System.out.print("Unknown warning");
+    }
+    System.out.println(" /!\\");
+
+  }
 }
